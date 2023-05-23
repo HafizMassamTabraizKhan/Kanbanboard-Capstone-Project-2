aff2d7acba04a8d7ac5005e4aa5e66e3ca6d9c2a
Massam Khan <hafizmassamtabraizkhan@gmail.com>
2023-05-23 16:58:56 +0500

    Complete html structure

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './style.css';
+import Logo from './assets/images/icpc-logo.png';
+
+const logoContainer = document.querySelector('.logo');
+
+const myLogo = new Image();
+myLogo.src = Logo;
+
+logoContainer.appendChild(myLogo);
