9408a8891e428f1e882ecbbebb3a76b77a5eac49
katfogy <katfogy@gmail.com>
2023-05-26 06:41:15 +0100

    comment and movies counter tests implemented

--- src/modules/moviesCounter.js
+++ src/modules/moviesCounter.js
@@ -1,8 +1,8 @@
 const movieCounters = () => {
-    const movies = document.querySelectorAll('.card');
-    const counter = document.getElementById('counter');
-    counter.innerText = movies.length;
-    return movies.length;
-  };
-  
-  export default movieCounters;
+  const movies = document.querySelectorAll('.card');
+  const counter = document.getElementById('counter');
+  counter.innerText = movies.length;
+  return movies.length;
+};
+
+export default movieCounters;
